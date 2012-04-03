8a164ac8a1f955e3bcd5f5d54e3d6a6e41248dc8
Fagner Martins <github@fagnermartins.com>
2012-04-03 20:06:38 -0300

    Reworked contains() method

--- src/utils.js
+++ src/utils.js
@@ -29,28 +29,16 @@ String.prototype.replaceAll = function(token, newToken, ignoreCase) {
 return str;
 };
 String.prototype.contains = function(token, ignoreCase) {
-     var _reg = 0;
-     
-     if(token) {
-	     var str = (ignoreCase === true)? this.toUpperCase(): this.toString();
-	     token = (ignoreCase === true)? token.toUpperCase(): token;
-	     
-	     //char
-	     if(str.length === 1) {
-	         for(var i = 0; i<str.length; i++) {
-	             if(str[i] == token) {
-	                 _reg++;
-	             }
-	         }
-	     } else {// substring
-	         var arr = [];
-	         var i = -1;
-	         while(str && (i = str.indexOf(token)) != -1) {
-	             arr.push(str.substring(i, i + token.length));
-	             str = str.substring(i + token.length);
-	         }
-	     _reg = arr.length;
-	     }
-     }
- return _reg;
- };
+	var _reg = 0, str = this.toString(), i;
+	if(str && typeof token === "string") {
+		if(ignoreCase === true) {
+			token = token.toLowerCase();
+			str = str.toLowerCase();
+		}
+		while((i = str.indexOf(token)) !== -1) {
+			str = str.substring(i + token.length);
+			_reg++;
+		}
+	}
+	return _reg;
+};
