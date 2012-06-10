1fc00516f9771212b936f7a1b5484c61f61d9ed9
Fagner Martins <github@fagnermartins.com>
2012-06-10 02:50:29 -0300

    1.0.2 Try to make the code style like the jQuery core (with a few mods) Change some internal data/bind names

--- src/jquery.tableFilter.js
+++ src/jquery.tableFilter.js
@@ -1,129 +1,153 @@
-/**
- * jQuery Table Filter 1.0.1 (jQuery 1.7+)
+/*!
+ * jQuery Table Filter 1.0.2 (jQuery 1.7+)
  * Fagner Martins Brack <fagnerbrack.com>
  * MIT license
+ * https://github.com/FagnerMartinsBrack/jQuery-Table-Filter
  * 
- * jQuery Table Filter
+ * Description:
  * Given a table with thead and tbody, jQuery Table Filter creates an input below all TDs of theads that automatically filters the content based on the innerHTML of each TD of the tbody
  * The TRs that don't match with desired filter are hidden and any input inside any TD is disabled (the input is enabled when the TR is visible again)
  * The filter verifies the content splitting each word as it is a different search ("A test" matches with "A - test" and "test A")
  * The only requirement for the use of this plugin is that it is used ONLY in a table with at least a <thead>, <th> and <tbody>
  * It is created a TR that informs the user when a filter did not return any result. It is positioned below the last TR of the tfoot. If there's no tfoot, jQuery Table Filter creates it.
  */
-(function($) {
+(function( $, undefined ) {
 	$.fn.tableFilter = function(args) {
 		var options = $.extend({
-			inputClasses: undefined, //custom classes that will be set in the filter inputs that will be created
-			trClasses: undefined, //custom classes that will be set in the TR(s) that will be created to contain the filters
-			thClasses: undefined, //custom classes that will be set in the TH(s) that will be created to contain the filters
-			delay: 1000, //delay to apply the filter after typing (if the table has a huge number of content)
-			ignoreCase: false, //Verifies if jQuery Table Filter should ignore the case
-			emptyMessage: "There's no result to show", //Message when there's no result
-			columns: [] //Index of the columns that will be filtered. If not especified consider all columns.
+			
+			//custom classes that will be set in the filter inputs that will be created
+			inputClasses: undefined,
+			
+			//custom classes that will be set in the TR(s) that will be created to contain the filters
+			trClasses: undefined,
+			
+			//custom classes that will be set in the TH(s) that will be created to contain the filters
+			thClasses: undefined,
+			
+			//delay to apply the filter after typing (if the table has a huge number of content)
+			delay: 1000,
+			
+			//Verifies if jQuery Table Filter should ignore the case
+			ignoreCase: false,
+			
+			//Message when there's no result
+			emptyMessage: "There's no result to show",
+			
+			//Index of the columns that will be filtered. If not especified consider all columns.
+			columns: []
 		}, args);
 		
 		function option(key) {
-			var ret = undefined;
-			if(key !== undefined) {
-				ret = options[key];
-			}
-		return ret;
-		};
+			return options[key];
+		}
 		
 		/**
 		 * @param colNumber The number of TDs in the table's thead
-		 * @return A jQuery reference to the new TR that will have the filters
+		 * @returns {jQuery} A jQuery reference to the new TR that will have the filters
 		 */
 		function getFilterTR(colNumber) {
-			var inputClasses = option('inputClasses');
-			var trClasses = option('trClasses');
-			var thClasses = option('thClasses');
-			var columns = option('columns');
-			var str = [];
-			str.push("<tr class='" + ($.type(trClasses) === 'string'? trClasses : '') + "'>");
-				for(var i = 0; i<colNumber; i++) {
-					str.push("<th class='" + ($.type(thClasses) === 'string'? thClasses : '') + "'>");
-						if(!$.isArray(columns) || !columns.length || columns.contains(i)) {
-							str.push("<input type='text' class='" + ($.type(inputClasses) === 'string'? inputClasses : '') + "'/>");
+			var i = 0,
+				inputClasses = option('inputClasses'),
+				trClasses = option('trClasses'),
+				thClasses = option('thClasses'),
+				columns = option('columns'),
+				str = [];
+			
+			str.push("<tr class='" + (typeof trClasses === "string" ? trClasses : "") + "'>");
+				for(; i<colNumber; i++) {
+					str.push("<th class='" + (typeof thClasses === "string" ? thClasses : "") + "'>");
+						if( !$.isArray(columns) || !columns.length || columns.contains(i) ) {
+							str.push("<input type='text' class='" + (typeof inputClasses === "string"? inputClasses : "") + "'/>");
 						}
 					str.push("</th>");
 				}
 			str.push("</tr>");
-		return $(str.join(''));
-		};
+		return $( str.join("") );
+		}
 		
 		/**
 		 * @param colNumber Greater number of TDs in the table (to set the colspan)
-		 * @return Um objeto jQuery reference to the TR that will appear in the footer when a filter shows no TR
+		 * @returns {jQuery} A jQuery reference to the TR that will appear in the footer when a filter shows no TR
 		 */
 		function getFooterTR(colNumber) {
 			var str = [];
 			str.push("<tr class='filter-info'>");
 				str.push("<td colspan='" + colNumber + "'>");
-					str.push(options.emptyMessage);
+					str.push( option("emptyMessage") );
 				str.push("</td>");
 			str.push("</tr>");
-		return $(str.join(''));
-		};
+		return $( str.join("") );
+		}
 		
 		/**
-		 * Set the .data() in the TDs the value if its index
-		 * So you dont need to verify the index via selector
+		 * Store the TD index to avoid a DOM check every time
 		 */
-		function markTDs() {
-			var $this = $(this);
-			$this.data('jQueryTableFilter_index', $this.index());
-		};
+		function eachTDs() {
+			var $td = $(this);
+			$td.data( "tablefilter-index", $td.index() );
+		}
 		
+		/**
+		 * Given a jQuery event object, return false if the key pressed matches with the map
+		 * 
+		 * @param {Object} The Event Object
+		 */
 		function acceptKey(e) {
-			var accept = true;
-			if(e.keyCode in {
-				16: 'shift',
-				17: 'control',
-				18: 'alt',
-				32: 'space'
-			}) {
-				accept = false;
-			}
-		return accept;
-		};
+			return !(e.keyCode in {
+				16: "shift",
+				17: "control",
+				18: "alt",
+				32: "space"
+			});
+		}
 		
 		/**
 		 * @param $customTR jQuery reference to the new TR that will have the filters
 		 * @param $bodyTRs jQuery reference to the table
+		 * 
+		 * @returns {jQuery} The $customTR as a jQuery Object
 		 */
-		function bind($customTR, $table) {
-			var $bodyTRs = $table.find('tbody tr');
-			var $bodyTDs = $bodyTRs.find('td').each(markTDs);
-			var $headTHs = $customTR.find('th');
-			var $allInputs = $headTHs.find('input');
-			var colNumber = $headTHs.length;
-			var ignoreCase = option('ignoreCase');
-			var delay = option('delay');
+		function bind( $customTR, $table ) {
+			var $bodyTRs = $table.find("tbody tr"),
+				$bodyTDs = $bodyTRs.find("td").each(eachTDs),
+				$headTHs = $customTR.find("th"),
+				$allInputs = $headTHs.find("input"),
+				colNumber = $headTHs.length,
+				ignoreCase = option("ignoreCase"),
+				delay = option("delay");
 			
-			$headTHs.each(function(index, element) {
-				var $TD = $(this);
-				var _applyFilter = function() {
-					var $TRsToHide = $();
+			$headTHs.each(function( index, element ) {
+				
+				function _applyFilter() {
+					var $tfoot, $infoTR,
+						$TRsToHide = $();
+					
 					show($bodyTRs);
 					
 					//Iterate between all inputs and filters each respective column
-					$allInputs.each(function(_i, el) {
-						var $input = $(el);
-						var inputVal = $input.val().trim();
-						var index;
+					$allInputs.each(function(_i) {
+						var index,
+							$input = $(this),
+							inputVal = $input.val().trim();
 						
 						if(inputVal) {
-							index = $input.parent().index(); //Get the index of the TD not the input (cause it is possible that not all TDs have inputs)
+							
+							//Get the index of the TD not the input (cause it is possible that not all TDs have inputs)
+							index = $input.parent().index();
+							
 							$bodyTDs.each(function() {
-								var $thisTD = $(this);
-								var _index = $thisTD.data('jQueryTableFilter_index');
-								if(_index === index) {
+								var $thisTD = $(this),
+									_index = $thisTD.data("tablefilter-index");
+								
+								if( _index === index ) {
 									var innerText = $thisTD.text().trim();
 									
 									//If it did'nt match...
-									if(!compare(innerText, inputVal, ignoreCase)) {
-										$TRsToHide = $TRsToHide.add(this.parentNode); //...Add to hide the TR of this TD
+									if( !compare(innerText, inputVal, ignoreCase) ) {
+										
+										//...Add to hide the TR of this TD
+										$TRsToHide = $TRsToHide.add( this.parentNode );
+										
 									}
 								}
 							});
@@ -134,157 +158,203 @@
 					hide($TRsToHide);
 					
 					//Verifies if there's no TR to show
-					var $tfoot = $table.find('tfoot:last');
-					if(!$tfoot.length) $tfoot = $('<tfoot />').appendTo($table.find('tbody:last').parent());
+					$tfoot = $table.find("tfoot:last");
+					if( !$tfoot.length ) {
+						$tfoot = $("<tfoot />").appendTo( $table.find('tbody:last').parent() );
+					}
 					
-					var $infoTR = $tfoot.find('tr.filter-info');
-					if($bodyTRs.filter(':visible').length === 0) {
-						if(!$infoTR.length) {
-							$tfoot.append(getFooterTR(colNumber)).show();
+					$infoTR = $tfoot.find("tr.filter-info");
+					if( !$bodyTRs.filter(":visible").length ) {
+						if( !$infoTR.length ) {
+							$tfoot.append( getFooterTR(colNumber) ).show();
 						} else {
 							$infoTR.show();
 						}
 					} else {
 						$infoTR.hide();
 					}
-				};
+				}
 				
 				//Get the input of this TD
 				//Change event to keyup. Sometimes a backspace keydown caused the input to be cleared after the code has been executed
-				$TD.find('input').bind('keyup.jQueryTableFilter', function(e) {
-					if(acceptKey(e)) { //Verifies if the typed key is acceptable in the filter
-						var $input = $(this);
-						var filterTimeout = $input.data('jQueryTableFilter_timeout');
+				$(this).find("input").bind( "keyup.tablefilter", function(e) {
+					if( acceptKey(e) ) { //Verifies if the typed key is acceptable in the filter
+						var $input = $(this),
+							filterTimeout = $input.data("tablefilter-timeout");
 						
-						if(filterTimeout !== undefined) {
+						if( filterTimeout !== undefined ) {
 							clearTimeout(filterTimeout);
 						}
 						
 						//Set the timeout only if the delay is greater than zero
-						if(delay > 0) {
-							$input.data('jQueryTableFilter_timeout', setTimeout(_applyFilter, delay));
+						if( delay > 0 ) {
+							$input.data( "tablefilter-timeout", setTimeout(_applyFilter, delay) );
 						} else {
 							_applyFilter();
 						}
 					}
 				});
 			});
-			return $customTR.data("jQueryTableFilter_tr", true);
-		};
+		return $customTR.data( "tablefilter-tr", true );
+		}
 		
+		/**
+		 * Add a flag to the TRs that will be hidden and hide it
+		 * 
+		 * @param {jQuery} A jQuery Object with the TRs to be hidden
+		 */
 		function hide($TRs) {
-			$TRs.not(':hidden').each(function() {
-				//Add a flag to the TRs that will be hidden and hide it
-				var $this = $(this);
-				$this.data('jqueryTableFilter_hide', true);
-				$this.find("input, select, textarea").each(function() {
-					var $_this = $(this);
-					if(!$_this.prop("disabled")) {
-						$_this.data("jQueryTableFilter_disabled", true).prop("disabled", true)
+			$TRs.not(":hidden").each(function() {
+				var $tr = $(this)
+					.data( "tablefilter-hide", true );
+				
+				$tr.find("input, select, textarea").each(function() {
+					var $input = $(this);
+					if( !$input.prop("disabled") ) {
+						$input.data( "tablefilter-disabled", true ).prop( "disabled", true );
 					}
 				});
-				$this.hide();
+				
+			$tr.hide();
 			});
-		};
+		}
 		
+		/**
+		 * Show the given TRs
+		 * 
+		 * @param {jQuery} A jQuery Object with the TRs to be hidden
+		 */
 		function show($TRs) {
 			$TRs.each(function() {
-				var $this = $(this);
+				var $tr = $(this);
 				
 				//Remove the flag of the hidden and disabled elements
-				$this.find("input, select, textarea").each(function() {
-					var $_this = $(this);
-					if($_this.data("jQueryTableFilter_disabled") === true) {
-						$_this.prop("disabled", false); //Just to clarify we should not use removeProp on native properties such as disabled
+				$tr.find("input, select, textarea").each(function() {
+					var $input = $(this);
+					if($input.data("tablefilter-disabled")) {
+						$input.prop( "disabled", false ); //Just to clarify we should not use removeProp on native properties such as disabled
 					}
 				});
 				
 				//Remove the flag of the hidden TRs and show it
-				if($this.data('jqueryTableFilter_hide') === true) {
-					$this.removeData('jqueryTableFilter_hide');
-					$this.show();
+				if($tr.data("tablefilter-hide")) {
+					$tr.removeData("tablefilter-hide");
+					$tr.show();
 				}
 			});
-		};
+		}
 		
+		/**
+		 * Separate the quoted search from the general search
+		 * 
+		 * @param {String} str The string to be tested
+		 * @returns {Array}
+		 */
 		function splitQuotes(str) {
-			var reg = new RegExp('([ ]?")([^"*]*)("[ ]?)', 'g');
-			var ret = [];
-			var matches = null;
-			while((matches = reg.exec(str)) !== null) {
+			var matches,
+				reg = new RegExp("([ ]?\")([^\"*]*)(\"[ ]?)", "g"),
+				ret = [];
+			
+			while( (matches = reg.exec(str)) !== null ) {
 				ret.push({
 					0: matches[0],
 					1: matches[2]
 				});
 			}
 		return ret;
-		};
+		}
 		
+		/**
+		 * Split to treat each search as a separate one
+		 * 
+		 * @param {String} values The content of the search
+		 * @returns {String} An array with each independent search
+		 */
 		function splitValues(values) {
 			
+			var i, trimmedVal,
+				ret = [],
+				quotes = splitQuotes(values);
+			
 			//Remove double quote
-			var quotes = splitQuotes(values);
-			for(var i = 0; i<quotes.length; i++) {
-				values = values.replaceAll(quotes[i][0], ' ');
+			for(i = 0; i<quotes.length; i++) {
+				values = values.replaceAll( quotes[i][0], " " );
 			}
 			
 			//Prepare the return
-			var trimmedVal = values.trim();
-			var ret = trimmedVal? trimmedVal.split(' ') : [];
-			for(var i = 0; i<quotes.length; i++) {
-				ret.push(quotes[i][0].trim());
+			if( trimmedVal = values.trim() ) {
+				ret = trimmedVal.split(" ");
 			}
 			
-			return ret;
-		};
+			for(i = 0; i<quotes.length; i++) {
+				ret.push( quotes[i][0].trim() );
+			}
+			
+		return ret;
+		}
 		
 		/**
 		 * Comparator method to check if the result of the filter matches with the column text
+		 * 
 		 * @param {String} text The innerHTML of the TD
 		 * @param {String} val The typed value to filter this column
 		 * @param {Boolean} ignoreCase If the compare should be case-sensitive
+		 * @returns {Boolean} If the values match with the compare rule within the given text
 		 */
-		function compare(text, values, ignoreCase) {
-			var valArray = splitValues(values);
-			var c = 0;
-			for(var i = 0; i<valArray.length; i++) {
-				var val = valArray[i];
-				var l = val.length;
-				if(l >= 2 && val.substring(0, 1) === '"' && val.substring(l - 1, l) === '"') {
+		function compare( text, values, ignoreCase ) {
+			var val, l,
+				c = 0,
+				i = 0,
+				valArray = splitValues(values);
+			
+			for(; i<valArray.length; i++) {
+				
+				val = valArray[i];
+				l = val.length;
+				
+				if ( l >= 2 && val.substring(0, 1) === "\"" && val.substring(l - 1, l) === "\"" ) {
+					
 					val = val.substring(1, l - 1);
-					if(contains(text, val, ignoreCase)) {
+					if( contains(text, val, ignoreCase) ) {
 						c++;
 					}
-				} else if(text.contains(val, ignoreCase)) {
+					
+				} else if( text.contains(val, ignoreCase) ) {
 					c++;
 				}
 			}
-			return c === valArray.length;
-		};
+		return c === valArray.length;
+		}
 		
 		/**
 		 * Check if the value matches with any word inside the string
+		 * 
+		 * @param {String} text The content of the TD
+		 * @param {String} val The value to be compared
+		 * @param {Boolean} ignoreCase if the compare should be made ignoring the case
+		 * @returns {Boolean} If the given val is contained inside the text
 		 */
-		function contains(text, val, ignoreCase) {
+		function contains( text, val, ignoreCase ) {
 			
-			function hasMatch(index) {
-				var matchLeft = index - 1 === -1 || text.substring(index - 1, index) === ' ';
-				var matchRight = index + val.length >= text.length || text.substring(index + val.length, index + val.length + 1) === ' ';
+			function _hasMatch(index) {
+				var matchLeft = index - 1 === -1 || text.substring( index - 1, index ) === " ",
+					matchRight = index + val.length >= text.length || text.substring( index + val.length, index + val.length + 1 ) === " ";
+				
 				return matchLeft && matchRight;
 			};
 			
-			return ignoreCase === true? hasMatch(text.toUpperCase().indexOf(val.toUpperCase())) : hasMatch(text.indexOf(val));
+			return ignoreCase ? _hasMatch( text.toUpperCase().indexOf(val.toUpperCase()) ) : _hasMatch( text.indexOf(val) );
 		};
 		
 		$(this).each(function() {
-			var $table = $(this);
-			var $thead = $table.find('thead:first');
-			var colNumber = $thead.find('tr:last th').length;
-			var $customTR = bind(getFilterTR(colNumber), $table);
-			var $lastHeadTR = $thead.find("tr:last");
+			var $table = $(this),
+				$thead = $table.find("thead:first"),
+				colNumber = $thead.find("tr:last th").length,
+				$customTR = bind( getFilterTR(colNumber), $table ),
+				$lastHeadTR = $thead.find("tr:last");
 			
-			//If a tr created by another plugin instance already exists, remove it.
-			if($lastHeadTR.data("jQueryTableFilter_tr") === true) {
+			//If a tr created by another instance of this plugin already exists, remove it.
+			if( $lastHeadTR.data("tablefilter-tr") ) {
 				$lastHeadTR.remove();
 			}
 			
